e0cd1302ed08f22a1cfdc55f8902037165b80886
TongLuong <tong.luong203@hcmut.edu.vn>
2024-04-06 22:00:38 +0700

    feat: build env

--- server/server/__init__.py
+++
@@ -1,5 +0,0 @@
-import pymysql
-import mysql.connector # type: ignore
-
-pymysql.install_as_MySQLdb()
-mysql.connector.connect()
--- server/server/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from api import views
-urlpatterns = [
-    # path("home/", include("home.urls")),
-    path('admin/', admin.site.urls),
-    path("api/User/", include("api.urls")),
-    path("", views.get_api),
-]
