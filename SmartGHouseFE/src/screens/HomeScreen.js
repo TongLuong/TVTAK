7be420d554f09e89d4f2128c1a6a9664a14bcda8
vxmtrung <vuxuanmaitrung1821@gmail.com>
2024-04-14 17:34:22 +0700

    home-page static basic done

--- SmartGHouseFE/src/screens/HomeScreen.js
+++ SmartGHouseFE/src/screens/HomeScreen.js
@@ -1,18 +1,102 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
-import styles from '../styles/styles';
+import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+// import styles from '../styles/styles';
 import { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { DataTable } from 'react-native-paper'
+import { DataTable, Modal } from 'react-native-paper';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const AppButton = ({ onPress, title }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      backgroundColor: 'white',
+      alignItems: 'center',
+      width: "56%",
+      alignSelf: 'center',
+      marginBottom: 10,
+      height: 26,
+      borderRadius: 20
+    }}
+  >
+    <Text style={{ fontSize: 15, fontVariant: 'roboto', color: '#3CAF58' }}>
+      {title}
+    </Text>
+  </TouchableOpacity>
+);
+
+const AppButton1 = ({ onPress, title }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      backgroundColor: 'white',
+      alignItems: 'center',
+      alignSelf: 'center',
+      justifyContent: 'center',
+      height: 26,
+      borderRadius: 20,
+      paddingLeft: 30,
+      paddingRight: 30
+    }}
+  >
+    <Text style={{ fontSize: 14, fontVariant: 'roboto', color: '#3CAF58' }}>
+      <Ionicons name='add-circle-outline' size={14} />
+      {" " + title}
+    </Text>
+  </TouchableOpacity>
+);
+
+const AppButton2 = ({ onPress, title }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      backgroundColor: 'white',
+      alignItems: 'center',
+      alignSelf: 'center',
+      justifyContent: 'center',
+      height: 26,
+      borderRadius: 20,
+      paddingLeft: 30,
+      paddingRight: 30,
+      marginTop: 15,
+      marginBottom: 15
+    }}
+  >
+    <Text style={{ fontSize: 14, fontVariant: 'roboto', color: '#3CAF58' }}>
+      {title}
+    </Text>
+  </TouchableOpacity>
+);
+
+const AppButton4 = ({ onPress, title }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      backgroundColor: 'white',
+      alignItems: 'center',
+      alignSelf: 'center',
+      justifyContent: 'center',
+      height: 26,
+      borderRadius: 20,
+      paddingLeft: 30,
+      paddingRight: 30,
+      marginTop: 15,
+      marginBottom: 15
+    }}
+  >
+    <Text style={{ fontSize: 14, fontVariant: 'roboto', color: '#3CAF58' }}>
+      {title}
+    </Text>
+  </TouchableOpacity>
+);
 
 export default function App() {
 
-  const navigation = useNavigation();
-  // const [currentTime, setCurrentTime] = useState('');
-  
   return (
   <>
-    <View style={{ margin: 10, borderColor: 'black', borderWidth: 1}}>
-      <View style={{ flexDirection: 'row', alignSelf: "center",justifyContent: 'space-around', alignItems: 'center', borderBottomWidth: 1, marginBottom: 10}}>
+
+    <View style={{ margin: 10, backgroundColor: '#EFF9F1', borderRadius: 45, padding: 10, marginTop: 20}}>
+      <View style={{ flexDirection: 'row', alignSelf: "center",justifyContent: 'space-around', alignItems: 'center', borderBottomWidth: 1, marginBottom: 10, borderColor: '#3CAF58'}}>
         <Text style={timeStyle.hours}>
           20:30{"  "}
         </Text>
@@ -21,7 +105,7 @@ export default function App() {
           ngày 15 tháng 5
         </Text>
       </View>
-      <View style={{ flexDirection: 'row', alignSelf: "center", justifyContent: 'space-around', alignItems: 'center', borderBottomWidth: 1, gap: 20, marginBottom: 15, paddingBottom: 10}}>
+      <View style={{ flexDirection: 'row', alignSelf: "center", justifyContent: 'space-around', alignItems: 'center', borderBottomWidth: 1, gap: 20, marginBottom: 15, paddingBottom: 10, borderColor: '#3CAF58'}}>
         <View style={{alignItems: 'center'}}>
           <Text style={statusStyle.title}>Độ ẩm{"\n"}</Text>
           <Text style={statusStyle.content}>69 %</Text>
@@ -35,77 +119,64 @@ export default function App() {
           <Text style={statusStyle.content}>85 Lux</Text> 
         </View>
       </View>
-      <Button
-        onPress={() => {
-          navigation.navigate('Manage')
-        }}
-        title="Learn More"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
+      <AppButton title={"Đi đến Quản lý nhà kính"} onPress={() => {navigation.navigate('Manage')}}/>
     </View>
 
-    <View style={{ margin: 10, borderColor: 'black', borderWidth: 1}}>
+    <View style={{ margin: 10, backgroundColor: '#EFF9F1', borderRadius: 45, paddingTop: 10}}>
     <DataTable>
       <DataTable.Row>
-        <DataTable.Cell>Thiết bị</DataTable.Cell>
+        <DataTable.Cell> <Text style={{fontSize: 23, color: '#3CAF58'}}> <Ionicons size={20} name="settings-sharp"/>  Thiết bị</Text></DataTable.Cell>
         <DataTable.Cell>
-          <Button
-          onPress={() => {
-            navigation.navigate('History')
-          }}
-          title="Custom Alert here"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-          />
+          <View style={{justifyContent: 'center', paddingBottom: 10}}>
+            <AppButton1 title={"Thêm thiết bị"} onPress={() => navigation.navigate('History')}/>
+          </View>
         </DataTable.Cell>
       </DataTable.Row>
-      <DataTable.Header style={styles.tableHeader}> 
-        <DataTable.Title style={{flex: 1.3}}>Tên thiết bị</DataTable.Title> 
-        <DataTable.Title>Chế độ</DataTable.Title> 
-        <DataTable.Title>Trạng thái</DataTable.Title>
+      <DataTable.Header> 
+        <DataTable.Cell style={{flex: 1.3}}>
+          <Text style={{color: '#3CAF58'}}>
+            Tên thiết bị
+          </Text>
+        </DataTable.Cell> 
+        <DataTable.Title>
+          <Text style={{color: '#3CAF58'}}>
+            Chế độ
+          </Text>
+        </DataTable.Title> 
+        <DataTable.Title>
+          <Text style={{color: '#3CAF58'}}>
+            Trạng thái
+          </Text>
+        </DataTable.Title>
       </DataTable.Header> 
       <DataTable.Row> 
-        <DataTable.Cell style={{flex: 1.3}}>Hệ thống tưới</DataTable.Cell> 
-        <DataTable.Cell>Thủ công</DataTable.Cell> 
-        <DataTable.Cell>Không hoạt động</DataTable.Cell>
+        <DataTable.Cell style={{flex: 1.3}} textStyle={{color: '#3CAF58'}}>Hệ thống tưới</DataTable.Cell> 
+        <DataTable.Cell textStyle={{color: '#3CAF58'}}>Thủ công</DataTable.Cell> 
+        <DataTable.Cell textStyle={{color: '#3CAF58'}}>Không hoạt động</DataTable.Cell>
       </DataTable.Row> 
   
       <DataTable.Row> 
-        <DataTable.Cell style={{flex: 1.3}}>Hệ thống chiếu sáng</DataTable.Cell> 
-        <DataTable.Cell>Tự động</DataTable.Cell> 
-        <DataTable.Cell>Đang hoạt động</DataTable.Cell>
+        <DataTable.Cell style={{flex: 1.3 }} textStyle={{color: '#3CAF58'}}>Hệ thống chiếu sáng</DataTable.Cell> 
+        <DataTable.Cell textStyle={{color: '#3CAF58'}}>Tự động</DataTable.Cell> 
+        <DataTable.Cell textStyle={{color: '#3CAF58'}}>Đang hoạt động</DataTable.Cell>
       </DataTable.Row> 
       <DataTable.Row> 
-      <DataTable.Cell style={{flex: 1.3}}>Cảm biến nhiệt độ</DataTable.Cell> 
-        <DataTable.Cell>Thủ công</DataTable.Cell> 
-        <DataTable.Cell>Không hoạt động</DataTable.Cell> 
+      <DataTable.Cell style={{flex: 1.3}} textStyle={{color: '#3CAF58'}}>Cảm biến nhiệt độ</DataTable.Cell> 
+        <DataTable.Cell textStyle={{color: '#3CAF58'}}>Thủ công</DataTable.Cell> 
+        <DataTable.Cell textStyle={{color: '#3CAF58'}}>Không hoạt động</DataTable.Cell> 
       </DataTable.Row>
       <DataTable.Row> 
-      <DataTable.Cell style={{flex: 1.3}}>Cảm biến độ ẩm</DataTable.Cell> 
-        <DataTable.Cell>Tự động</DataTable.Cell> 
-        <DataTable.Cell>Đang hoạt động</DataTable.Cell>
+      <DataTable.Cell style={{flex: 1.3}} textStyle={{color: '#3CAF58'}}>Cảm biến độ ẩm</DataTable.Cell> 
+        <DataTable.Cell textStyle={{color: '#3CAF58'}}>Tự động</DataTable.Cell> 
+        <DataTable.Cell textStyle={{color: '#3CAF58'}}  >Đang hoạt động</DataTable.Cell>
       </DataTable.Row>
 
       <DataTable.Row>
-        <DataTable.Cell style={{flex: 1.4}}>
-        <Button
-        onPress={() => {
-        }}
-        title="Custom device here"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
+        <DataTable.Cell style={{flex: 1.1}} textStyle={{color: '#3CAF58'}}>
+        <AppButton2 title={"Cài đặt thiết bị"}/>
         </DataTable.Cell>
-        <DataTable.Cell style={{flex: 1.1}}>
-        <Button
-        onPress={() => {
-          navigation.navigate('History')
-        }}
-        title="Move to another page"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
+        <DataTable.Cell style={{flex: 1.4}}>
+          <AppButton4 title={"Xem lịch sử hoạt động"} onPress={() => {navigation.navigate('History')}}/>
         </DataTable.Cell>
       </DataTable.Row>
     </DataTable> 
@@ -119,11 +190,13 @@ export default function App() {
     {
       hours: {
         fontSize: 55,
-        alignSelf: 'center'
+        alignSelf: 'center',
+        color: '#3CAF58',
       },
       date: {
         fontSize: 24,
-        alignSelf: 'center'
+        alignSelf: 'center',
+        color: '#3CAF58'
       }
     }
   )
@@ -131,10 +204,12 @@ export default function App() {
   const statusStyle = StyleSheet.create({
     title: {
       fontSize: 18,
-      alignSelf: 'center'
+      alignSelf: 'center',
+      color: '#3CAF58'
     },
     content: {
       fontSize: 37,
-      alignSelf: 'center'
+      alignSelf: 'center',
+      color: '#3CAF58'
     }
   })
