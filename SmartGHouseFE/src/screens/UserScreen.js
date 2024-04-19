1b9e7259d500224dc7ecb65ee42cb73a9d4c9038
an114116 <131609524+an114116@users.noreply.github.com>
2024-04-19 23:53:54 +0700

    Update UserScreen.js

--- SmartGHouseFE/src/screens/UserScreen.js
+++ SmartGHouseFE/src/screens/UserScreen.js
@@ -3,18 +3,20 @@ import LoginScreen from './LoginScreen'
 import SignupScreen from './SignUpScreen'
 import AuthopScreen from './AuthOptionScreen'
 import AccountScreen from './AccountScreen'
+import ForgetPass1Screen from './ForgetPass1Screen'
+import ForgetPass2Screen from './ForgetPass2Screen'
 
 const Stack = createStackNavigator();
 
 export default function App() {
     return (
       <Stack.Navigator>
-        <Stack.Screen name="Authop" component={AuthopScreen} />
+        <Stack.Screen name="User" component={AuthopScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Forget pass 1" component={ForgetPass1Screen}/>
+        <Stack.Screen name="Forget pass 2" component={ForgetPass2Screen}/>
         <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Account" component={AccountScreen} />
       </Stack.Navigator>
     );
   }
-  
-  
