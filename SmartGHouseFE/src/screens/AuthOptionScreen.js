0298743a98a09b91dbdd2ef353be8aec7d3fd901
an114116 <131609524+an114116@users.noreply.github.com>
2024-04-19 23:51:44 +0700

    Update AuthOptionScreen.js

--- SmartGHouseFE/src/screens/AuthOptionScreen.js
+++ SmartGHouseFE/src/screens/AuthOptionScreen.js
@@ -1,16 +1,35 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function App({navigation}){
   return (
-    <View style = {{flexDirection:'row', alignItems:'center', marginTop:60}}>
-      <Image style = {{width: 50, height: 50, borderRadius: 100}} source={require('../Image/User_avatar.png')}/>
+    <View style = {authStyle.authMain}>
+      <Ionicons name = 'person-circle' size = {60}/>
       <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-        <Text style = {{fontVariant:'roboto' ,fontWeight:'bold', fontSize:30, color:'#3CAF58'}}>Đăng nhập</Text>
+        <Text style = {authStyle.optionText}>Đăng nhập</Text>
       </TouchableOpacity>
-      <Text style = {{fontVariant:'roboto' ,fontWeight:'bold', fontSize:40, color:'#3CAF58'}}>/</Text>
+      <Text style = {authStyle.optionText}>/</Text>
       <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
-        <Text style = {{fontVariant:'roboto' ,fontWeight:'bold', fontSize:30, color:'#3CAF58'}}>Đăng Ký</Text>
+        <Text style = {authStyle.optionText}>Đăng Ký</Text>
       </TouchableOpacity>
     </View>
   );
 }
+
+const authStyle = StyleSheet.create(
+    {
+      authMain: {
+        flexDirection:'row', 
+        alignItems:'center', 
+        backgroundColor: '#EFF9F1', 
+        flex:1, 
+        marginLeft: 10
+      },
+      optionText:{
+        fontVariant:'roboto' ,
+        fontWeight:'bold', 
+        fontSize:30, 
+        color:'#3CAF58'
+      },
+    }
+  )
