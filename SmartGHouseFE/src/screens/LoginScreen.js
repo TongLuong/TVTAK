eead60b810323e82d04ad5dfbe4a80a70498286c
TongLuong <tong.luong203@hcmut.edu.vn>
2024-04-20 12:48:22 +0700

    feat: call api login (not done)

--- SmartGHouseFE/src/screens/LoginScreen.js
+++ SmartGHouseFE/src/screens/LoginScreen.js
@@ -1,7 +1,39 @@
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useForm } from "react-hook-form";
+import axiosInst from "../axios/axiosClient";
+import { useState } from 'react';
+import axios from "axios";
 
 export default function App({navigation}) {
+    const {register, handleSubmit} = useForm({
+      defaultValues: {username: "", password: ""}
+    });
+
+    const [username, setUsername] = useState("");
+    const [pass, setPass] = useState("");
+
+    const onSignInSubmit = async () => {
+      // await axiosInst.get(
+      //   "/api/user/signin",
+      //   {
+      //     account: username,
+      //     password: pass
+      //   })
+      //   .then(res => {
+      //     console.log(res.data);
+      //   })
+      //   .catch(e => console.log(e));
+      console.log("yo");
+      await axiosInst.get(
+        "test")
+        .then(res => {
+          console.log("hello");
+        })
+        .catch(e => console.log(e));
+        console.log("yo2");
+    };
+
     return (
         <SafeAreaView style = {loginStyle.loginMain}>
           <View style = {loginStyle.title}>
@@ -12,6 +44,8 @@ export default function App({navigation}) {
             <View>
               <Text style = {{fontVariant:'roboto' ,fontWeight:'regular', color:'black'}}>Địa chỉ email</Text>
               <TextInput
+                //{...register("username", { required: "field cannot be empty" })}
+                onChangeText={(text) => setUsername(text)}
                 style = {loginStyle.input}
                 placeholder=''
                 placeholderTextColor = 'black'
@@ -20,13 +54,19 @@ export default function App({navigation}) {
             <View>
               <Text style = {{fontVariant:'roboto' ,fontWeight:'regular', color:'black'}}>Mật khẩu</Text>
               <TextInput 
+                //{...register("password")}
+                onChangeText={(text) => setPass(text)}
                 style = {loginStyle.input}
                 placeholder=''
                 placeholderTextColor = 'black'
               />
             </View>
             <View>
-              <TouchableOpacity style = {loginStyle.submitButton} onPress={() => navigation.navigate('Account')}>
+              <TouchableOpacity style = {loginStyle.submitButton}
+                onPress={() => {
+                  //navigation.navigate('Account')}
+                  onSignInSubmit()
+                  }}>
                 <Text style={ loginStyle.submitButtonText}>Đăng nhập</Text>
               </TouchableOpacity>
             </View>
--- server/src/main/java/com/tvtak/tvtak/TvtakApplication.java
+++ server/src/main/java/com/tvtak/tvtak/TvtakApplication.java
@@ -2,13 +2,23 @@ package com.tvtak.tvtak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
+@CrossOrigin
 public class TvtakApplication 
 {
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(TvtakApplication.class, args);
 	}
+
+	@GetMapping("/test")
+    public ResponseEntity<Object> test()
+    {
+        return new ResponseEntity<>("testing", HttpStatus.OK);
+    }
 }
 
