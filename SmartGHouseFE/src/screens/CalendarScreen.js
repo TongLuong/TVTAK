637c054eb1f2507b1e936fbb99d87a891a360b04
vxmtrung <vuxuanmaitrung1821@gmail.com>
2024-04-24 10:24:26 +0700

    Revert "update toi lam tiep"
    This reverts commit 6b634fc18b0ae57566a48a170ae693e6028c48c0.

--- SmartGHouseFE/src/screens/CalendarScreen.js
+++ SmartGHouseFE/src/screens/CalendarScreen.js
@@ -3,44 +3,11 @@ import styles from '../styles/styles';
 
 export default function App() {
     return (
-<View style={[styles.container, {marginTop: 88,marginLeft: 15, marginRight: 15, backgroundColor:'#EFF9F1', borderRadius: 30}]}>
-        
-        <DataTable>
-             <DataTable.Row>
-               <DataTable.Cell> <Text style={{fontSize: 23, color: '#3CAF58'}}> Lịch sử thiết bị</Text></DataTable.Cell>
-               <DataTable.Cell>
-                 <View style={{justifyContent: 'center', paddingBottom: 10}}>
-                 </View>
-               </DataTable.Cell>Ư
-             </DataTable.Row>
-             <DataTable.Header> 
-               <DataTable.Cell>
-                 <Text style={{color: '#3CAF58'}}>
-                   Ngày
-                 </Text>
-               </DataTable.Cell> 
-               <DataTable.Title>
-                 <Text style={{color: '#3CAF58'}}>
-                   Giờ
-                 </Text>
-               </DataTable.Title> 
-               <DataTable.Title>
-                 <Text style={{color: '#3CAF58'}}>
-                   Nội dung
-                 </Text>
-               </DataTable.Title>
-             </DataTable.Header> 
-             <DataTable.Row> 
-               <DataTable.Cell style={{flex: 1}} textStyle={{color: '#3CAF58'}}>14/04/2024</DataTable.Cell> 
-               <DataTable.Cell style={{flex: 0.8}} textStyle={{color: '#3CAF58'}}>07:00</DataTable.Cell> 
-               <DataTable.Cell style={{flex: 1.2}} textStyle={{color: '#3CAF58'}}>Hệ thống đèn</DataTable.Cell>
-               <DataTable.Cell textStyle={{color: '#3CAF58'}}>Tắt</DataTable.Cell>
-             </DataTable.Row> 
-       
-           </DataTable> 
-         
-             </View>
-       
+      <View style={styles.container}>
+
+        <Text style={styles.baseText}>Welcome to Calendar Screen</Text>
+  
+      </View>
     );
   }
   
