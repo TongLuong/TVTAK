fb694bd2307d2e4712482fc2bbea4a3a5682df2c
vxmtrung <vuxuanmaitrung1821@gmail.com>
2024-04-25 23:31:04 +0700

    Base calendar done :D

--- SmartGHouseFE/src/axios/axiosClient.js
+++ SmartGHouseFE/src/axios/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInst = axios.create({
-    // baseURL: "http://192.168.2.56:8080",
-    baseURL: "https://tvtak.onrender.com",
+    baseURL: "http://192.168.0.108:8080",
+    // baseURL: "https://tvtak.onrender.com",
     timeout: 5000,
     headers: {
         "Content-Type": "application/json"
--- SmartGHouseFE/src/screens/CalendarScreen.js
+++ SmartGHouseFE/src/screens/CalendarScreen.js
@@ -1,13 +1,228 @@
-import { StyleSheet, Text, View } from 'react-native';
-import styles from '../styles/styles';
+import React, { useState, useRef, useEffect } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  TouchableWithoutFeedback,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  Touchable,
+} from 'react-native';
+import moment from 'moment';
+import Swiper from 'react-native-swiper';
+
+const { width } = Dimensions.get('window');
 
 export default function App() {
-    return (
+  const swiper = useRef();
+  const [value, setValue] = useState(new Date());
+  const [week, setWeek] = useState(0);
+
+  const weeks = React.useMemo(() => {
+    const start = moment().add(week, 'weeks').startOf('week');
+
+    return [-1, 0, 1].map(adj => {
+      return Array.from({ length: 7 }).map((_, index) => {
+        const date = moment(start).add(adj, 'week').add(index, 'day');
+
+        return {
+          weekday: date.format('ddd'),
+          date: date.toDate(),
+        };
+      });
+    });
+  }, [week]);
+
+  // useEffect(() => 
+  // {
+  //   setCount(count + 1)
+  // }, [value])
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
 
-        <Text style={styles.baseText}>Welcome to Calendar Screen</Text>
-  
+        <View style={styles.picker}>
+          <Swiper
+            index={1}
+            ref={swiper}
+            loop={false}
+            showsPagination={false}
+            onIndexChanged={ind => {
+              if (ind === 1) {
+                return;
+              }
+              else {
+                setTimeout(() => {
+                  const newIndex = ind - 1;
+                  const newWeek = week + newIndex;
+                  setWeek(newWeek);
+                  setValue(moment(value).add(newIndex, 'week').toDate());
+                  swiper.current.scrollTo(1, false);
+                }, 100);
+              }
+            }}>
+            {weeks.map((dates, index) => (
+              <View
+                style={[styles.itemRow, { paddingHorizontal: 16 }]}
+                key={index}>
+                {dates.map((item, dateIndex) => {
+                  const isActive =
+                    value.toDateString() === item.date.toDateString();
+                  return (
+                    <TouchableWithoutFeedback
+                      key={dateIndex}
+                      onPress={() => setValue(item.date)}>
+                      <View
+                        style={[
+                          styles.item,
+                          isActive && {
+                            backgroundColor: '#9CDD9B',
+                          },
+                        ]}>
+                        <Text
+                          style={[
+                            styles.itemWeekday,
+                            isActive && { color: 'black' },
+                          ]}>
+                          {item.weekday}
+                        </Text>
+                        <Text
+                          style={[
+                            styles.itemDate,
+                            isActive && { color: 'black' },
+                          ]}>
+                          {item.date.getDate()}
+                        </Text>
+                      </View>
+                    </TouchableWithoutFeedback>
+                  );
+                })}
+              </View>
+            ))}
+          </Swiper>
+        </View>
+
+      <View style = {{ backgroundColor: '#EFF9F1', marginTop:'5%' }}>
+        <Text>Thời gian thu hoạch dự kiến</Text>
       </View>
-    );
+
+      <View style = {{ backgroundColor: '#EFF9F1', marginTop:'5%' }}>
+        <Text>Thông báo đã đặt</Text>
+      </View>
+
+      <View style = {{ backgroundColor: '#EFF9F1', marginTop:'5%' }}>
+        <Text>Ghi chú</Text>
+      </View>
+
+      </View>
+    </SafeAreaView>
+  );
   }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingVertical: 24,
+      paddingTop: '20%'
+    },
+    header: {
+      paddingHorizontal: 16,
+    },
+    title: {
+      fontSize: 32,
+      fontWeight: '700',
+      color: '#1d1d1d',
+      marginBottom: 12,
+    },
+    picker: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: '#EFF9F1',
+      justifyContent: 'center',
+      height: '20%'
+    },
+    subtitle: {
+      fontSize: 17,
+      fontWeight: '600',
+      color: '#999999',
+      marginBottom: 12,
+    },
+    footer: {
+      marginTop: 'auto',
+      paddingHorizontal: 16,
+    },
+    /** Item */
+    item: {
+      flex: 1,
+      height: '100%',
+      marginHorizontal: 4,
+      paddingVertical: 6,
+      paddingHorizontal: 4,
+      borderWidth: 1,
+      borderRadius: 30,
+      borderColor: '#e3e3e3',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#CAFFCF',
+      elevation: 5,
+    },
+    itemRow: {
+      width: width,
+      height: '86%',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      justifyContent: 'space-between',
+    },
+    itemWeekday: {
+      fontSize: 13,
+      fontWeight: '500',
+      color: '#737373',
+      marginBottom: 4,
+    },
+    itemDate: {
+      fontSize: 15,
+      fontWeight: '600',
+      color: '#111',
+    },
+    /** Placeholder */
+    placeholder: {
+      flexGrow: 1,
+      flexShrink: 1,
+      flexBasis: 0,
+      height: 400,
+      marginTop: 0,
+      padding: 0,
+      backgroundColor: 'transparent',
+    },
+    placeholderInset: {
+      borderWidth: 4,
+      borderColor: '#e5e7eb',
+      borderStyle: 'dashed',
+      borderRadius: 9,
+      flexGrow: 1,
+      flexShrink: 1,
+      flexBasis: 0,
+    },
+    /** Button */
+    btn: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 8,
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderWidth: 1,
+      backgroundColor: '#007aff',
+      borderColor: '#007aff',
+    },
+    btnText: {
+      fontSize: 18,
+      lineHeight: 26,
+      fontWeight: '600',
+      color: '#fff',
+    },
+  });
   
