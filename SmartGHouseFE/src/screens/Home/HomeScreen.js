6d304b2f01cc56b23e41ea3ef69dbdab317c24f7
vuongnguyen <trungvuong2169@gmail.com>
2024-04-29 01:18:38 +0700

    fix: home, history, create chart page

---
+++ SmartGHouseFE/src/screens/Home/HomeScreen.js
@@ -0,0 +1,376 @@
+import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+// import styles from '../styles/styles';
+import { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { DataTable, Modal } from "react-native-paper";
+import { getRecordLast } from "../../services/userService";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { MaterialCommunityIcons, FontAwesome5 } from "@expo/vector-icons";
+import { path } from "../../utils/constants";
+const AppButton = ({ onPress, title }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      backgroundColor: "white",
+      alignItems: "center",
+      width: "46%",
+      alignSelf: "center",
+      marginBottom: 10,
+      maxHeight: 26,
+      borderRadius: 20,
+    }}
+  >
+    <Text
+      style={{
+        fontSize: 16,
+        fontVariant: "roboto",
+        color: "#3CAF58",
+        paddingVertical: "1%",
+      }}
+    >
+      {title}
+    </Text>
+  </TouchableOpacity>
+);
+
+const AppButton1 = ({ onPress, title }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      backgroundColor: "white",
+      alignItems: "center",
+      alignSelf: "center",
+      justifyContent: "space-between",
+      maxHeight: 26,
+      borderRadius: 20,
+      paddingLeft: 30,
+      paddingRight: 30,
+    }}
+  >
+    <Text style={{ fontSize: 16, fontVariant: "roboto", color: "#3CAF58" }}>
+      <Ionicons
+        style={{ alignSelf: "center" }}
+        name="add-circle-outline"
+        size={12}
+      />
+      {" " + title}
+    </Text>
+  </TouchableOpacity>
+);
+
+const AppButton2 = ({ onPress, title }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      backgroundColor: "white",
+      alignItems: "center",
+      alignSelf: "center",
+      justifyContent: "center",
+      height: 26,
+      borderRadius: 20,
+      paddingLeft: 30,
+      paddingRight: 30,
+    }}
+  >
+    <Text style={{ fontSize: 12, fontVariant: "roboto", color: "#3CAF58" }}>
+      {title}
+    </Text>
+  </TouchableOpacity>
+);
+
+const AppButton4 = ({ onPress, title }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      backgroundColor: "white",
+      alignItems: "center",
+      alignSelf: "center",
+      justifyContent: "center",
+      height: 26,
+      borderRadius: 20,
+      paddingLeft: 30,
+      paddingRight: 30,
+    }}
+  >
+    <Text style={{ fontSize: 12, fontVariant: "roboto", color: "#3CAF58" }}>
+      {title}
+    </Text>
+  </TouchableOpacity>
+);
+
+export default function HomeScreen({ navigation }) {
+  const [humidity, setHumidity] = useState("0");
+  const [temp, setTemp] = useState("0");
+  const [light, setLight] = useState("0");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const responseHumid = await getRecordLast("iot-humid");
+        setHumidity(responseHumid?.data?.value);
+        const responseTemp = await getRecordLast("iot-temp");
+        setTemp(responseTemp?.data?.value);
+        const responseLight = await getRecordLast("iot-light");
+        setLight(responseLight?.data?.value);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <View style={{ paddingTop: "5%" }}>
+      <View
+        style={{
+          margin: 10,
+          backgroundColor: "#EFF9F1",
+          borderRadius: 45,
+          padding: 10,
+          marginTop: 20,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 23,
+            color: "#3CAF58",
+            alignSelf: "center",
+            marginBottom: 10,
+          }}
+        >
+          Trạng thái hệ thống
+        </Text>
+        <View
+          style={{
+            flexDirection: "row",
+            alignSelf: "center",
+            justifyContent: "space-around",
+            alignItems: "center",
+            gap: 20,
+            marginBottom: 15,
+            paddingBottom: 10,
+            borderColor: "#3CAF58",
+          }}
+        >
+          <View style={dataStyle.container}>
+            <View style={{ alignSelf: "center" }}>
+              <Ionicons name="water-outline" size={24} color="white" />
+            </View>
+            <Text style={statusStyle.content}>{humidity}%</Text>
+            <Text style={statusStyle.title}>Độ ẩm{"\n"}</Text>
+          </View>
+          <View style={dataStyle.container}>
+            <View style={{ alignSelf: "center" }}>
+              <MaterialCommunityIcons
+                name="thermometer"
+                size={24}
+                color="orange"
+              />
+            </View>
+            <Text style={statusStyle.content}>{temp}°</Text>
+            <Text style={statusStyle.title}>Nhiệt độ{"\n"}</Text>
+          </View>
+          <View style={dataStyle.container}>
+            <View style={{ alignSelf: "center" }}>
+              <FontAwesome5 name="lightbulb" size={24} color="yellow" />
+            </View>
+            <Text style={statusStyle.content}>{light} Lux</Text>
+            <Text style={statusStyle.title}>Cường độ sáng{"\n"}</Text>
+          </View>
+        </View>
+        <AppButton
+          title={"Thống kê dữ liệu"}
+          onPress={() => {
+            navigation.navigate(path.CHART);
+          }}
+        />
+      </View>
+
+      <View
+        style={{
+          margin: 10,
+          marginTop: 0,
+          height: "100%",
+          backgroundColor: "#EFF9F1",
+          borderRadius: 45,
+          paddingTop: 10,
+        }}
+      >
+        <DataTable>
+          <DataTable.Row>
+            <DataTable.Cell>
+              {" "}
+              <Text style={{ fontSize: 23, color: "#3CAF58" }}>
+                {" "}
+                <Ionicons size={20} name="settings-sharp" /> Thiết bị
+              </Text>
+            </DataTable.Cell>
+            <DataTable.Cell>
+              <View
+                style={{
+                  justifyContent: "center",
+                  paddingBottom: 10,
+                  width: "100%",
+                }}
+              >
+                <AppButton1
+                  title={"Thêm"}
+                  onPress={() => navigation.navigate("History")}
+                />
+              </View>
+            </DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Header>
+            <DataTable.Cell style={{ flex: 3 }}>
+              <Text style={{ color: "#3CAF58" }}>Tên thiết bị</Text>
+            </DataTable.Cell>
+            <DataTable.Title style={{ flex: 2 }}>
+              <Text style={{ color: "#3CAF58" }}>Chế độ</Text>
+            </DataTable.Title>
+            <DataTable.Title style={{ flex: 1 }}>
+              <Text style={{ color: "#3CAF58" }}>Trạng thái</Text>
+            </DataTable.Title>
+          </DataTable.Header>
+          <DataTable.Row>
+            <DataTable.Cell
+              style={{ flex: 3 }}
+              textStyle={{ color: "#3CAF58" }}
+            >
+              Hệ thống tưới
+            </DataTable.Cell>
+            <DataTable.Cell
+              style={{ flex: 2 }}
+              textStyle={{ color: "#3CAF58" }}
+            >
+              Thủ công
+            </DataTable.Cell>
+            <DataTable.Cell
+              style={{ flex: 1 }}
+              textStyle={{ color: "#3CAF58" }}
+            >
+              Tắt
+            </DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+            <DataTable.Cell
+              style={{ flex: 3 }}
+              textStyle={{ color: "#3CAF58" }}
+            >
+              Hệ thống chiếu sáng
+            </DataTable.Cell>
+            <DataTable.Cell
+              style={{ flex: 2 }}
+              textStyle={{ color: "#3CAF58" }}
+            >
+              Tự động
+            </DataTable.Cell>
+            <DataTable.Cell
+              style={{ flex: 1 }}
+              textStyle={{ color: "#3CAF58" }}
+            >
+              Bật
+            </DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell
+              style={{ flex: 3 }}
+              textStyle={{ color: "#3CAF58" }}
+            >
+              Cảm biến nhiệt độ
+            </DataTable.Cell>
+            <DataTable.Cell
+              style={{ flex: 2 }}
+              textStyle={{ color: "#3CAF58" }}
+            >
+              Thủ công
+            </DataTable.Cell>
+            <DataTable.Cell
+              style={{ flex: 1 }}
+              textStyle={{ color: "#3CAF58" }}
+            >
+              Tắt
+            </DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell
+              style={{ flex: 3 }}
+              textStyle={{ color: "#3CAF58" }}
+            >
+              Cảm biến độ ẩm
+            </DataTable.Cell>
+            <DataTable.Cell
+              style={{ flex: 2 }}
+              textStyle={{ color: "#3CAF58" }}
+            >
+              Tự động
+            </DataTable.Cell>
+            <DataTable.Cell
+              style={{ flex: 1 }}
+              textStyle={{ color: "#3CAF58" }}
+            >
+              Bật
+            </DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row style={{ padding: "3%" }}>
+            <DataTable.Cell
+              style={{ position: "absolute", left: "10%" }}
+              textStyle={{ color: "#3CAF58" }}
+            >
+              <AppButton2 title={"Cài đặt"} />
+            </DataTable.Cell>
+            <DataTable.Cell style={{ position: "absolute", right: "10%" }}>
+              <AppButton4
+                title={"Lịch sử"}
+                onPress={() => {
+                  navigation.navigate("History");
+                }}
+              />
+            </DataTable.Cell>
+          </DataTable.Row>
+        </DataTable>
+      </View>
+    </View>
+  );
+}
+
+const timeStyle = StyleSheet.create({
+  hours: {
+    fontSize: 40,
+    alignSelf: "center",
+    color: "#3CAF58",
+    paddingRight: 10,
+  },
+  date: {
+    fontSize: 24,
+    alignSelf: "center",
+    color: "#3CAF58",
+  },
+});
+
+const statusStyle = StyleSheet.create({
+  title: {
+    fontSize: 12,
+    alignSelf: "center",
+    color: "white",
+  },
+  content: {
+    fontSize: 20,
+    alignSelf: "center",
+    color: "white",
+  },
+});
+const dataStyle = StyleSheet.create({
+  container: {
+    backgroundColor: "#49796B", // Fallback color if gradients are not supported
+    // backgroundImage: 'linear-gradient(315deg, #55efc4 0%, #000000 74%)',
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 20,
+    borderColor: "black",
+    borderWidth: 1,
+    flex: 1,
+  },
+  measureLine: {},
+});
--- SmartGHouseFE/src/services/userService.js
+++ SmartGHouseFE/src/services/userService.js
@@ -1,93 +1,126 @@
 import axiosInst from "../axios/axiosClient";
 
-
 const signIn = async (account, pass) => {
-    return await axiosInst.get(`/api/user/signin?account=${account}&password=${pass}`)
-                            .catch(e => console.log(e))
-}
+  return await axiosInst
+    .get(`/api/user/signin?account=${account}&password=${pass}`)
+    .catch((e) => console.log(e));
+};
 const signUp = async (body) => {
-    return await axiosInst.post("/api/user/signup", body)
-}
+  return await axiosInst.post("/api/user/signup", body);
+};
 
-const createSchedule = async(user_id, device_id, schedule) => {
-    return await axiosInst.post(`/api/schedule/create-schedule?user_id=${user_id}&device_id=${device_id}`, schedule)
-}
-const getAllSchedule = async(user_id) => {
-    return await axiosInst.get(`/api/schedule/get-all-schedules?user_id=${user_id}`)
-}
-const deleteSchedule = async(user_id, schedule_id) => {
-    return await axiosInst.delete(`/api/schedule/delete-schedule?user_id=${user_id}&schedule_id=${schedule_id}`)
-}
-const updateSchedule = async(user_id, schedule_id, schedule) => {
-    return await axiosInst.post(`/api/schedule/edit-schedule?user_id=${user_id}&schedule_id=${schedule_id}`, schedule)
-}
-const createNewNote = async(user_id, note) => {
-    return await axiosInst.post(`/api/note/create-new-note?user_id=${user_id}`, note)
-}
-const deleteNote = async(user_id, note_id) => {
-    return await axiosInst.delete(`/api/note/delete-note?user_id=${user_id}&note_id=${note_id}`)
-}
-const getAllNote = async(user_id) => {
-    return await axiosInst.get(`/api/note/get-all-notes?user_id=${user_id}`)
-}
-const editNote = async(user_id, note_id, note) => {
-    return await axiosInst.post(`/api/note/edit-note?user_id=${user_id}&note_id=${note_id}`, note)
-}
-const createNotification = async(user_id, notification) => {
-    return await axiosInst.post(`/api/notification/create-notification?user_id=${user_id}`, notification)
-}
-const getAllNotification = async(user_id) => {
-    return await axiosInst.get(`/api/notification/get-all-notifications?user_id=${user_id}`)
-}
-const deleteNotification = async(user_id, notification_id) => {
-    return await axiosInst.delete(`/api/notification/delete-notification?user_id=${user_id}&notification_id=${notification_id}`)
-}
+const createSchedule = async (user_id, device_id, schedule) => {
+  return await axiosInst.post(
+    `/api/schedule/create-schedule?user_id=${user_id}&device_id=${device_id}`,
+    schedule
+  );
+};
+const getAllSchedule = async (user_id) => {
+  return await axiosInst.get(
+    `/api/schedule/get-all-schedules?user_id=${user_id}`
+  );
+};
+const deleteSchedule = async (user_id, schedule_id) => {
+  return await axiosInst.delete(
+    `/api/schedule/delete-schedule?user_id=${user_id}&schedule_id=${schedule_id}`
+  );
+};
+const updateSchedule = async (user_id, schedule_id, schedule) => {
+  return await axiosInst.post(
+    `/api/schedule/edit-schedule?user_id=${user_id}&schedule_id=${schedule_id}`,
+    schedule
+  );
+};
+const createNewNote = async (user_id, note) => {
+  return await axiosInst.post(
+    `/api/note/create-new-note?user_id=${user_id}`,
+    note
+  );
+};
+const deleteNote = async (user_id, note_id) => {
+  return await axiosInst.delete(
+    `/api/note/delete-note?user_id=${user_id}&note_id=${note_id}`
+  );
+};
+const getAllNote = async (user_id) => {
+  return await axiosInst.get(`/api/note/get-all-notes?user_id=${user_id}`);
+};
+const editNote = async (user_id, note_id, note) => {
+  return await axiosInst.post(
+    `/api/note/edit-note?user_id=${user_id}&note_id=${note_id}`,
+    note
+  );
+};
+const createNotification = async (user_id, notification) => {
+  return await axiosInst.post(
+    `/api/notification/create-notification?user_id=${user_id}`,
+    notification
+  );
+};
+const getAllNotification = async (user_id) => {
+  return await axiosInst.get(
+    `/api/notification/get-all-notifications?user_id=${user_id}`
+  );
+};
+const deleteNotification = async (user_id, notification_id) => {
+  return await axiosInst.delete(
+    `/api/notification/delete-notification?user_id=${user_id}&notification_id=${notification_id}`
+  );
+};
 
 const addDevice = async (user_id, device) => {
-    return await axiosInst.post(`api/device/add-device?user_id=${user_id}`, device)
-}
-const getAllDevice = async(user_id) => {
-    return await axiosInst.get(`/api/device/get-all-devices?user_id=${user_id}`)
-}
+  return await axiosInst.post(
+    `api/device/add-device?user_id=${user_id}`,
+    device
+  );
+};
+const getAllDevice = async (user_id) => {
+  return await axiosInst.get(`/api/device/get-all-devices?user_id=${user_id}`);
+};
 const deleteDevice = async (user_id, device_id) => {
-    return await axiosInst.delete(`api/device/delete-device?user_id=${user_id}&device_id=${device_id}`)
-}
+  return await axiosInst.delete(
+    `api/device/delete-device?user_id=${user_id}&device_id=${device_id}`
+  );
+};
 const toggleDevice = async (user_id, device_id, status) => {
-    return await axiosInst.post(`api/device/toggle-device?user_id=${user_id}&device_id=${device_id}&status=${status}`)
-    
-}
+  return await axiosInst.post(
+    `api/device/toggle-device?user_id=${user_id}&device_id=${device_id}&status=${status}`
+  );
+};
 
 const getRecord = async (device_name) => {
-    return await axiosInst.get(`api/record/get-data?feed_id=${device_name}`)
-}
+  console.log(device_name);
+  return await axiosInst.get(`api/record/get-data?feed_id=${device_name}`);
+};
 const getRecordLast = async (device_name) => {
-    return await axiosInst.get(`api/record/get-data-last?feed_id=${device_name}`)
-}
+  return await axiosInst.get(`api/record/get-data-last?feed_id=${device_name}`);
+};
 
 const getDataByMonth = async (device_name, month, year) => {
-    return await axiosInst.get(`api/record/get-data-by?feed_id=${device_name}&month=${month}&year=${year}`)
-}
+  return await axiosInst.get(
+    `api/record/get-data-by?feed_id=${device_name}&month=${month}&year=${year}`
+  );
+};
 
 export {
-    signIn,
-    signUp,
-    createSchedule,
-    getAllSchedule,
-    deleteSchedule,
-    updateSchedule,
-    createNewNote,
-    deleteNote,
-    getAllNote,
-    editNote,
-    createNotification,
-    getAllNotification,
-    deleteNotification,
-    addDevice,
-    getAllDevice,
-    deleteDevice,
-    toggleDevice,
-    getRecord,
-    getRecordLast,
-    getDataByMonth
-    
-}
+  signIn,
+  signUp,
+  createSchedule,
+  getAllSchedule,
+  deleteSchedule,
+  updateSchedule,
+  createNewNote,
+  deleteNote,
+  getAllNote,
+  editNote,
+  createNotification,
+  getAllNotification,
+  deleteNotification,
+  addDevice,
+  getAllDevice,
+  deleteDevice,
+  toggleDevice,
+  getRecord,
+  getRecordLast,
+  getDataByMonth,
+};
