2720de2e0d773d07da53e0f11a5ac4bdf5d48ec3
vuongnguyen <trungvuong2169@gmail.com>
2024-05-03 23:06:02 +0700

    fix: screen login and account

--- SmartGHouseFE/src/axios/axiosClient.js
+++ SmartGHouseFE/src/axios/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInst = axios.create({
-  baseURL: "http://192.168.2.56:8080",
+  baseURL: "http://192.168.1.10:8080",
   // baseURL: "https://tvtak.onrender.com",
   timeout: 10000,
   headers: {
--- SmartGHouseFE/src/screens/Authen/LoginScreen.js
+++ SmartGHouseFE/src/screens/Authen/LoginScreen.js
@@ -1,142 +1,194 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useState } from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { signIn } from '../../services/userService';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-export default function App({navigation}) {
-    const [username, setUsername] = useState("");
-    const [pass, setPass] = useState("");
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { signIn } from "../../services/userService";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+export default function App({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [pass, setPass] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-    // -1: login failed, 1: login succeeded, 0: not done
-    const [loginStatus, setLoginStatus] = useState(0);
-
-    const onSignInSubmit = async () => {
-      try {
-        const res = await signIn(username, pass);
-        if (res.status === 200) {
-          setLoginStatus(1);
-          navigation.navigate('HomeScreen');
-          const userDataString = JSON.stringify(res.data);
-          await AsyncStorage.setItem('User', userDataString);
-        }
-      } catch (error) {
-        console.log(error);
-        setLoginStatus(-1);
+  // -1: login failed, 1: login succeeded, 0: not done
+  const [loginStatus, setLoginStatus] = useState(0);
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const onSignInSubmit = async () => {
+    try {
+      const res = await signIn(username, pass);
+      if (res.status === 200) {
+        setLoginStatus(1);
+        navigation.navigate("HomeScreen");
+        const userDataString = JSON.stringify(res.data);
+        await AsyncStorage.setItem("User", userDataString);
       }
-    };
-
-    return (
-        <SafeAreaView style = {loginStyle.loginMain}>
-          <View style = {loginStyle.title}>
-            <Ionicons name = 'person-circle' size = {60}/>
-            <Text style = {loginStyle.titleText}>Đăng nhập</Text>
-          </View>
-          <View style = {{flexDirection:'column'}}>
-            <View>
-              <Text style = {loginStyle.inputTitle}>Tên đăng nhập hoặc email</Text>
-              <TextInput
-                onChangeText={(text) => setUsername(text)}
-                style = {loginStyle.input}
-                placeholder=''
-                placeholderTextColor = 'black'
-              />
-            </View>
-            <View>
-              <Text style = {loginStyle.inputTitle}>Mật khẩu</Text>
-              <TextInput
-                onChangeText={(text) => setPass(text)}
-                style = {loginStyle.input}
-                placeholder=''
-                placeholderTextColor = 'black'
-              />
-            </View>
-            <View style = {{marginTop: 10}}>
-              {loginStatus == -1 && <Text style = {loginStyle.failedText}>Sai tên đăng nhập hoặc mật khẩu!</Text>}
-              <TouchableOpacity style = {loginStyle.submitButton}
-                onPress={() => {
-                  onSignInSubmit()
-                  }}>
-                <Text style={ loginStyle.submitButtonText}>Đăng nhập</Text>
-              </TouchableOpacity>
-            </View>
-            <View style = {loginStyle.otherfunction}>
-              <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
-                <Text style = {loginStyle.otherfunctionText}>Đăng ký</Text>
-              </TouchableOpacity>
-              <Text style = {{flex:1}}/>
-              <TouchableOpacity onPress={() => navigation.navigate('Forget pass 1')}>
-                <Text style = {loginStyle.otherfunctionText}>Quên mật khẩu</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </SafeAreaView>
-    );
-  }
-  const loginStyle = StyleSheet.create(
-    {
-      loginMain: {
-        backgroundColor:'white',
-        paddingHorizontal:30,
-        borderColor: 'black',
-        borderWidth: 1,
-        margin: 10,
-        height: 500,
-        marginTop: 100,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        borderBottomLeftRadius: 20,
-        borderBottomRightRadius: 20,
-      },
-      title:{
-        marginTop: 30,
-        flex: 1,
-        alignItems: 'center',
-        flexDirection: 'column'
-      },
-      titleText:{
-        fontVariant:'roboto' ,
-        fontWeight:'bold', 
-        fontSize:40, 
-        color:'#3CAF58'
-      },
-      input: {
-        margin: 15,
-        height: 40,
-        borderColor: 'black',
-        borderWidth: 1
-      },
-      inputTitle:{
-        fontVariant:'roboto' ,
-        fontWeight:'regular', 
-        color:'black'
-      },
-      failedText:{
-        fontVariant:'roboto',
-        fontWeight:'regular',
-        textAlign: 'center',
-        color:'red'
-      },
-      submitButton: {
-        backgroundColor: '#3CAF58',
-        padding: 10,
-        margin: 15,
-        height: 40,
-        borderColor: '#3CAF58',
-        alignItems:'center',
-        borderWidth: 1
-      },
-      submitButtonText: {
-        color: 'white'
-      },
-      otherfunction: {
-        marginBottom: 20,
-        flexDirection: 'row'
-      },
-      otherfunctionText: {
-        fontVariant:'roboto' ,
-        fontWeight:'regular', 
-        color:'#3CAF58', 
-      },
+    } catch (error) {
+      console.log(error);
+      setLoginStatus(-1);
     }
-  )
+  };
+
+  return (
+    <SafeAreaView style={loginStyle.loginMain}>
+      <View style={loginStyle.title}>
+        <Ionicons name="person-circle" size={80} />
+        <Text style={loginStyle.titleText}>Welcome</Text>
+      </View>
+      <View style={{ flexDirection: "column" }}>
+        <View>
+          <Text style={loginStyle.inputTitle}>Tên đăng nhập hoặc email</Text>
+          <TextInput
+            onChangeText={(text) => setUsername(text)}
+            style={loginStyle.input}
+            placeholder="Nhap ten dang nhap hoac email"
+            placeholderTextColor="#aaa"
+          />
+        </View>
+        <View>
+          <Text style={loginStyle.inputTitle}>Mật khẩu</Text>
+          <View style={loginStyle.containerPass}>
+            <TextInput
+              secureTextEntry={!showPassword}
+              onChangeText={(text) => setPass(text)}
+              style={loginStyle.inputPass}
+              placeholder="Nhap mat khau"
+              placeholderTextColor="#aaa"
+            />
+            <MaterialCommunityIcons
+              name={showPassword ? "eye-off" : "eye"}
+              size={24}
+              color="#aaa"
+              style={loginStyle.iconPass}
+              onPress={toggleShowPassword}
+            />
+          </View>
+        </View>
+        <View style={{ marginTop: 10 }}>
+          {loginStatus == -1 && (
+            <Text style={loginStyle.failedText}>
+              Sai tên đăng nhập hoặc mật khẩu!
+            </Text>
+          )}
+          <TouchableOpacity
+            style={loginStyle.submitButton}
+            onPress={() => {
+              onSignInSubmit();
+            }}
+          >
+            <Text style={loginStyle.submitButtonText}>Đăng nhập</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={loginStyle.signupButton}
+            onPress={() => navigation.navigate("Signup")}
+          >
+            <Text style={loginStyle.submitButtonText}>Đăng Ký</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+const loginStyle = StyleSheet.create({
+  loginMain: {
+    backgroundColor: "white",
+    paddingHorizontal: 30,
+    borderColor: "black",
+    borderWidth: 1,
+    margin: 10,
+    height: 600,
+    marginTop: 100,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  containerPass: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 40,
+    margin: 15,
+    backgroundColor: "#f3f3f3",
+    borderRadius: 5,
+  },
+  title: {
+    marginTop: 30,
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  titleText: {
+    fontVariant: "roboto",
+    fontWeight: "bold",
+    fontSize: 40,
+    color: "#3CAF58",
+  },
+  input: {
+    margin: 15,
+    height: 40,
+    // borderColor: "black",
+    // borderWidth: 1,
+    paddingLeft: 12,
+    borderRadius: 5,
+    backgroundColor: "#f3f3f3",
+  },
+  inputTitle: {
+    fontVariant: "roboto",
+    fontWeight: "bold",
+    color: "black",
+    marginLeft: 15,
+  },
+  failedText: {
+    fontVariant: "roboto",
+    fontWeight: "regular",
+    textAlign: "center",
+    color: "red",
+  },
+  submitButton: {
+    backgroundColor: "#3CAF58",
+    padding: 10,
+    margin: 15,
+    height: 40,
+    borderColor: "#3CAF58",
+    alignItems: "center",
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  signupButton: {
+    backgroundColor: "#bdbdbd",
+    padding: 10,
+    margin: 15,
+    height: 40,
+    borderColor: "#bdbdbd",
+    alignItems: "center",
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  submitButtonText: {
+    color: "white",
+  },
+  otherfunction: {
+    marginBottom: 20,
+    flexDirection: "row",
+  },
+  otherfunctionText: {
+    fontVariant: "roboto",
+    fontWeight: "regular",
+    color: "#3CAF58",
+  },
+  iconPass: {
+    marginRight: 15,
+  },
+  inputPass: {
+    flex: 1,
+    paddingLeft: 12,
+  },
+});
