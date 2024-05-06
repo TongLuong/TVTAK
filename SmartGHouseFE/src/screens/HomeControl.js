692c9b0b6aacb157f54605d436359da2040a852f
vuongnguyen <trungvuong2169@gmail.com>
2024-05-06 18:21:57 +0700

    feat(chart): fix chart and api

--- SmartGHouseFE/src/screens/HomeControl.js
+++ SmartGHouseFE/src/screens/HomeControl.js
@@ -14,11 +14,11 @@ export default function App() {
         component={HomeScreen}
         options={{ headerShown: false }}
       />
-      {/* <Stack.Screen
+      <Stack.Screen
         name={path.CHART}
         component={ChartScreen}
         options={{ headerShown: false }}
-      /> */}
+      />
       <Stack.Screen
         name={"DeviceAddtion"}
         component={DeviceAddition}
